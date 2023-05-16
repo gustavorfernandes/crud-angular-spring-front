997b4e304eafad66c5cb2776fe2c21698fd4d1f3
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-16 09:27:53 -0300

    feat: :art: adding table component and refactoring material imports module

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { MaterialModule } from '../shared/imports/material/material.module';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
-  imports: [CommonModule, CoursesRoutingModule],
+  declarations: [CoursesComponent],
+  imports: [CommonModule, CoursesRoutingModule, MaterialModule],
 })
 export class CoursesModule {}

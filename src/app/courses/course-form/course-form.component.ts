b365c71f4465a48216a8db0abd373bc11acb7e01
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-25 10:01:24 -0300

    chore: :sparkles: adding create usecase in courses service

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -8,7 +11,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private courserService: CoursesService,
+    private snackBar: MatSnackBar
+  ) {}
 
   form: FormGroup = this.formBuilder.group({
     name: [null],
@@ -20,6 +28,13 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
-    console.log('Submeteu o formulário')
+    this.courserService.create(this.form.value).subscribe({
+      next: (result) => console.log(result),
+      error: (error) => this.onError(),
+    });
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
   }
 }

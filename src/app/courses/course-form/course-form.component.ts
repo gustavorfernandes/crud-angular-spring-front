9c0c6ba6179eba6759b7a2442852e6a1953eaa92
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-25 09:26:16 -0300

    feat: :sparkles: adding addCourses form component

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
+  form: FormGroup = this.formBuilder.group({
+    name: [null],
+    category: [null],
+  });
+
+  onCancel() {
+    this.router.navigate(['']);
+  }
+
+  onSubmit() {
+    console.log('Submeteu o formulário')
+  }
 }
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CoursesComponent } from './courses/courses.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { MaterialModule } from '../shared/imports/material/material.module';
 import { SharedModule } from '../shared/shared.module';
-import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [CoursesComponent, CourseFormComponent],
-  imports: [CommonModule, CoursesRoutingModule, MaterialModule, SharedModule],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class CoursesModule {}

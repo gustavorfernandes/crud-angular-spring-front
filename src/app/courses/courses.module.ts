f2c50241f3c12b6a921dd591513b16f78eccde14
GustavoFernandes <gustavo.ribeiro.fernandes1@gmail.com>
2023-05-28 10:28:42 -0300

    refactor: :recycle: organizing application folder and file structure

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { CoursesComponent } from './courses/courses.component';
-import { CourseFormComponent } from './course-form/course-form.component';
+import { CoursesComponent } from './containers/courses/courses.component';
+import { CourseFormComponent } from './containers/course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { MaterialModule } from '../shared/imports/material/material.module';
 import { SharedModule } from '../shared/shared.module';
-import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 @NgModule({
   declarations: [CoursesComponent, CourseFormComponent, CoursesListComponent],

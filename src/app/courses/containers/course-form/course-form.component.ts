bb57188110d53495a5c8db0d114702a43b0a1108
Gustavo <gustavo.ribeiro.fernandes1@gmail.com.br>
2023-06-05 18:29:27 -0300

    feat: :sparkles: adding form validators at course-form-component

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -23,8 +23,11 @@ export class CourseFormComponent {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: [''],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
+    ],
+    category: ['', Validators.required],
   });
 
   onCancel() {
@@ -38,13 +41,37 @@ export class CourseFormComponent {
     });
   }
 
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
+    this.onCancel();
+  }
+
   private onError() {
     this.snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
   }
 
-  private onSuccess() {
-    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
-    this.onCancel();
+  public getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors
+        ? field.errors['minlength']['requiredLength']
+        : 3;
+      return `O campo deve ter no mínimo ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors
+        ? field.errors['maxlength']['requiredLength']
+        : 250;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Formato inválido.';
   }
 
   ngOnInit(): void {
